8e41c6a86f0b17a34abe83072a9777db710fb231
Md Johirul Islam <johirbuet@gmail.com>
2017-10-26 12:26:22 -0500

    small change

---
+++ matploExample.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 26 12:16:23 2017
+
+@author: mislam
+"""
+
+from numpy import sin, pi, arange
+from appjar import gui 
+import random
+
+def getXY():
+    x = arange(0.0, 3.0, 0.01)
+    y = sin(random.randint(1,10) * pi * x)
+    return x,y
+
+def generate(btn):
+    # *getXY() will unpack the two return values
+    # and pass them as separate parameters
+    app.updatePlot("p1", *getXY())
+    showLabels()
+
+def showLabels():
+    axes.legend(['The curve'])
+    axes.set_xlabel("X Axes")
+    axes.set_ylabel("Y Axes")
+    app.refreshPlot("p1")
+
+app = gui()
+axes = app.addPlot("p1", *getXY())
+showLabels()
+app.addButton("Generate", generate)
+app.go()
