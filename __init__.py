fb263f449cf6b86dfb3e8ff87f0b9d1a54ddadc3
johirbuet <johirbuet@gmail.com>
2017-10-26 11:14:07 -0500

    first window

--- __init__.py
+++ __init__.py
@@ -4,7 +4,7 @@ from appjar import SelectableLabel
 from appjar import AutoCompleteEntry
 from appjar import ajScale
 from appjar import AjText, AjScrolledText
-from aappjar import Meter
+from appjar import Meter
 from appjar import Properties
 from appjar import Link
 from appjar import Separator
--- test.py
+++ test.py
@@ -1,2 +1,15 @@
-import __init__
+from appjar import gui
 app = gui()
+app.addLabel("title", "Welcome to appJar")
+app.setLabelBg("title", "red")
+app.addLabelEntry("Username")
+app.addLabelSecretEntry("Password")
+def press(button):
+    if button == "Cancel":
+        app.stop()
+    else:
+        usr = app.getEntry("Username")
+        pwd = app.getEntry("Password")
+        print("User:", usr, "Pass:", pwd)
+app.addButtons(["Submit", "Cancel"], press)
+app.go()
